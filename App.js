6a0a847e4eb3f805058001b113d17f35fad70b58
Pablo Romo <pabsromo@gmail.com>
2022-01-08 10:37:38 -0600

    initial

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  var datum;
+  
+  const fetchApi = async () => {
+    console.log('inside fetchapi');
+    try {
+      const res = await axios.get('http://104.2.186.154:8096');
+      console.log(res.data);
+      datum = res.data;
+    } catch (error) {
+      console.log(error.message);
+    }
+    // const Http = new XMLHttpRequest();
+    // const url='http://104.2.186.154:8096';
+    // Http.open("GET", url);
+    // Http.send();
+
+    // Http.onreadystatechange = (e) => {
+    // console.log(Http.responseText)
+    // data = Http.responseText.toString();
+// }
+  }
+
+  useEffect(() => {
+    console.log('using fetchApi');
+    fetchApi()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{datum}</Text>
       <StatusBar style="auto" />
     </View>
   );
